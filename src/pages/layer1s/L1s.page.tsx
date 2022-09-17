a6ce2eeb4de6d543ffb34ef653d5b1102709c7f0
hiljis <henric.hiljanen@gmail.com>
2022-09-17 21:55:53 +0200

    Integrated profiles store in L1sPage

--- src/pages/layer1s/L1s.page.tsx
+++ src/pages/layer1s/L1s.page.tsx
@@ -1,11 +1,15 @@
 import L1sGrid from '../../sections/layer1sGrid/L1sGrid.section';
 import L1sWhat from '../../sections/layer1sWhat/L1sWhat.section';
+import { useAppSelector } from '../../store/hooks';
+import { selectProfiles } from '../../store/profiles/profilesSlice';
 import './L1s.page.scss';
 
 const L1sPage: React.FC = () => {
+	const profiles = useAppSelector(selectProfiles);
+	const tickers = profiles.map((profile) => profile.ticker);
 	return (
 		<main className="layer1sGridPage">
-			<L1sGrid />
+			<L1sGrid tickers={tickers} />
 			<L1sWhat />
 		</main>
 	);
--- src/sections/layer1sGrid/L1sGrid.section.tsx
+++ src/sections/layer1sGrid/L1sGrid.section.tsx
@@ -1,31 +1,11 @@
 import L1Card from '../../components/l1Card/L1Card';
 import './L1sGrid.section.scss';
 
-const tickers = [
-	'btc',
-	'eth',
-	'bnb',
-	'ada',
-	'sol',
-	'avax',
-	'ftm',
-	'flow',
-	'xlm',
-	'rune',
-	'eos',
-	'dot',
-	'matic',
-	'trx',
-	'near',
-	'algo',
-	'icp',
-	'hbar',
-	'qnt',
-	'xtz',
-	'egld',
-];
+type Props = {
+	tickers: string[];
+};
 
-const L1sGrid: React.FC = () => {
+const L1sGrid: React.FC<Props> = ({ tickers }) => {
 	return (
 		<section className="section__layer1s">
 			<h4 className="section__header section__header--layer1sGrid">Layer 1 protocols</h4>
