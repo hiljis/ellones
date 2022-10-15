011c73839c11a04110821fec3b35094809673a76
hiljis <henric.hiljanen@gmail.com>
2022-10-15 16:51:15 +0200

    Added responsiveness to HeroPresentation

--- src/pages/l1Presentation/L1Presentation.page.tsx
+++ src/pages/l1Presentation/L1Presentation.page.tsx
@@ -20,7 +20,7 @@ const L1PresentationPage: React.FC = () => {
 	}, [ticker]);
 
 	return (
-		<main className="l1PresentationPage">
+		<main className={`l1PresentationPage ${ticker}`}>
 			<HeroPresentation ticker={ticker} />
 		</main>
 	);
--- src/sections/heroPresentation/HeroPresentation.section.tsx
+++ src/sections/heroPresentation/HeroPresentation.section.tsx
@@ -64,100 +64,35 @@ const HeroPresentation: React.FC<Props> = ({ ticker }) => {
 
 	return (
 		<section className={`heroPresentation ${ticker}`}>
-			<div className="heroPresentation--left">
+			<article className="heroPresentation__article">
 				<div className="heroPresentation__headerBox">
 					<h2 className="heroPresentation__header">{capFirst(profile.name)}</h2>
 					{getIcon(ticker, 'icon--sm icon--white')}
 				</div>
+
 				<p className="heroPresentation__text">{profile.longDescript}</p>
 				<button className="btn btn--invert btn--invert--white" type="button">
 					{removeHttps(profile.website)}
 				</button>
-				<h5 className={`heroPresentation__giantTicker ${ticker}`}>{ticker}</h5>
+			</article>
+
+			<h5 className={`heroPresentation__giantTicker ${ticker}`}>{ticker}</h5>
+
+			<div className="heroPresentation__statGrid">
+				<StatBox title={'Market Cap'} unit={mCapUnit} number={mCap} />
+				<StatBox title={'Price'} unit={price} number={'$'} />
+				<StatBox title={'Volume'} unit={volumeUnit} number={volume} />
+				<StatBox title={'Security Model'} unit={''} number={profile.chainType} />
+				<StatBox title={'Circ. Supply'} unit={circSupplyUnit} number={circSupply} />
+				<StatBox title={'Max Supply'} unit={maxSupplyUnit}>
+					{parseInt(maxSupply) === 0 ? <>&infin;</> : maxSupply}
+				</StatBox>
+				<StatBox title={'Genesis'} number={genesisYear} />
+				<StatBox title={'Decentralization'} unit="/ 10" number={profile.dScore} />
 			</div>
 
-			<div className="heroPresentation--right">
-				<div className="heroPresentation__statGrid">
-					<StatBox title={'Market Cap'} unit={mCapUnit} number={mCap} />
-					<StatBox title={'Price'} unit={price} number={'$'} />
-					<StatBox title={'Volume'} unit={volumeUnit} number={volume} />
-					<StatBox title={'Security Model'} unit={''} number={profile.chainType} />
-					<StatBox title={'Circ. Supply'} unit={circSupplyUnit} number={circSupply} />
-					<StatBox title={'Max Supply'} unit={maxSupplyUnit}>
-						{parseInt(maxSupply) === 0 ? <>&infin;</> : maxSupply}
-					</StatBox>
-					<StatBox title={'Genesis'} number={genesisYear} />
-					<StatBox title={'Decentralization'} unit="/ 10" number={profile.dScore} />
-					{/* <div className={`heroPresentation__statBox heroPresentation__statBox--${ticker}`}>
-						<p className="heroPresentation__stat">
-							<span className="heroPresentation__stat--number">{mCap}</span>
-							<span className="heroPresentation__stat--unit">{mCapUnit}</span>
-						</p>
-						<p className="heroPresentation__statTitle">Market Cap</p>
-					</div> */}
-
-					{/* <div className={`heroPresentation__statBox heroPresentation__statBox--${ticker}`}>
-						<p className="heroPresentation__stat">
-							<span className="heroPresentation__stat--unit">{'$'}</span>
-							<span className="heroPresentation__stat--number">{price}</span>
-						</p>
-						<p className="heroPresentation__statTitle">Price</p>
-					</div> */}
-
-					{/* <div className={`heroPresentation__statBox heroPresentation__statBox--${ticker}`}>
-						<p className="heroPresentation__stat">
-							<span className="heroPresentation__stat--number">{volume}</span>
-							<span className="heroPresentation__stat--unit">{volumeUnit}</span>
-						</p>
-						<p className="heroPresentation__statTitle">Volume</p>
-					</div> */}
-
-					{/* <div className={`heroPresentation__statBox heroPresentation__statBox--${ticker}`}>
-						<p className="heroPresentation__stat">
-							<span className="heroPresentation__stat--number">{profile.chainType}</span>
-							<span className="heroPresentation__stat--unit"></span>
-						</p>
-						<p className="heroPresentation__statTitle">Security Model</p>
-					</div> */}
-
-					{/* <div className={`heroPresentation__statBox heroPresentation__statBox--${ticker}`}>
-						<p className="heroPresentation__stat">
-							<span className="heroPresentation__stat--number">{circSupply}</span>
-							<span className="heroPresentation__stat--unit">{circSupplyUnit}</span>
-						</p>
-						<p className="heroPresentation__statTitle">Circ. Supply</p>
-					</div> */}
-
-					{/* <div className={`heroPresentation__statBox heroPresentation__statBox--${ticker}`}>
-						<p className="heroPresentation__stat">
-							<span className="heroPresentation__stat--number">
-								{parseInt(maxSupply) === 0 ? <>&infin;</> : maxSupply}
-							</span>
-							<span className="heroPresentation__stat--unit">{maxSupplyUnit}</span>
-						</p>
-						<p className="heroPresentation__statTitle">Max Supply</p>
-					</div> */}
-
-					{/* <div className={`heroPresentation__statBox heroPresentation__statBox--${ticker}`}>
-						<p className="heroPresentation__stat">
-							<span className="heroPresentation__stat--number">{genesisYear}</span>
-							<span className="heroPresentation__stat--unit"></span>
-						</p>
-						<p className="heroPresentation__statTitle">Genesis</p>
-					</div> */}
-
-					{/* <div className={`heroPresentation__statBox heroPresentation__statBox--${ticker}`}>
-						<p className="heroPresentation__stat">
-							<span className="heroPresentation__stat--number">{profile.dScore}</span>
-							<span className="heroPresentation__stat--unit">/ 10</span>
-						</p>
-						<p className="heroPresentation__statTitle">Decentralization</p>
-					</div> */}
-				</div>
-
-				<div className={`heroPresentation__chart ${ticker}`}>
-					<PresentationChart ticker={ticker} />
-				</div>
+			<div className={`heroPresentation__chart ${ticker}`}>
+				<PresentationChart ticker={ticker} />
 			</div>
 		</section>
 	);
