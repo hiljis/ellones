ca87b0d050d786adff2f92bd321da86ffd02cc37
hiljis <henric.hiljanen@gmail.com>
2022-10-17 20:40:23 +0200

    Added sign in and account listitem to BurgerNav depending on if there is a currentUser

--- src/sections/header/burgerNav/BurgerNav.tsx
+++ src/sections/header/burgerNav/BurgerNav.tsx
@@ -2,6 +2,8 @@ import { ReactComponent as IconCaretDown } from '../../../assets/svg/icon_caret-
 import { NavLink } from 'react-router-dom';
 import './BurgerNav.scss';
 import { useState } from 'react';
+import { useAppSelector } from '../../../store/hooks';
+import { selectCurrentUser } from '../../../store/user/userSlice';
 
 type Props = {
 	open: boolean;
@@ -9,6 +11,7 @@ type Props = {
 };
 
 const BurgerNav: React.FC<Props> = ({ open, closeHandler }) => {
+	const currentUser = useAppSelector(selectCurrentUser);
 	const [dropdownOpen, setDropdownOpen] = useState(false);
 	const handleClose = () => {
 		closeHandler();
@@ -19,6 +22,19 @@ const BurgerNav: React.FC<Props> = ({ open, closeHandler }) => {
 	return (
 		<nav className={`burgerNav ${open ? 'open' : ''}`}>
 			<ul className="burgerNav__list">
+				{currentUser ? (
+					<li className="burgerNav__listItem">
+						<NavLink to="/account" className="burgerNav__link" onClick={handleClose}>
+							my profile
+						</NavLink>
+					</li>
+				) : (
+					<li className="burgerNav__listItem">
+						<NavLink to="/signin" className="burgerNav__link" onClick={handleClose}>
+							sign in
+						</NavLink>
+					</li>
+				)}
 				<li className="burgerNav__listItem">
 					<NavLink to="/charts" className="burgerNav__link" onClick={handleClose}>
 						charts
@@ -30,7 +46,10 @@ const BurgerNav: React.FC<Props> = ({ open, closeHandler }) => {
 					</NavLink>
 				</li>
 				<li className="burgerNav__listItem">
-					<div className={`burgerNav__dropdownButton ${dropdownOpen ? 'open' : ''}`} onClick={handleToggleDropdown}>
+					<div
+						className={`burgerNav__dropdownButton ${dropdownOpen ? 'open' : ''}`}
+						onClick={handleToggleDropdown}
+					>
 						<span className="burgerNav__dropdownButtonContent">
 							market
 							<IconCaretDown className="burgerNav__dropdownButtonIcon" />
